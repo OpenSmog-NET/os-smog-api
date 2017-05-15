4ce932ac742ff7e1cac5ca23980d641514981214
Maciej Misztal <mmisztal1980@gmail.com>
2017-05-16 01:48:59 +0200

    #12 Invoker xml-docs

--- src/OS.Events.Streamstone/Invoker.cs
+++ src/OS.Events.Streamstone/Invoker.cs
@@ -12,11 +12,24 @@ namespace OS.Events.Streamstone
     {
         private const int SliceSize = 1000;
 
+        /// <summary>
+        /// Retrieves the partition
+        /// </summary>
+        /// <param name="table">The Azure Table Storage</param>
+        /// <param name="key">Partitioning key (aggregateId)</param>
+        /// <returns></returns>
         public static Partition GetPartition(this CloudTable table, string key)
         {
             return new Partition(table, key);
         }
 
+        /// <summary>
+        /// Retrieves the partition
+        /// </summary>
+        /// <param name="table">The Azure Table Storage</param>
+        /// <param name="key">Partitioning key (aggregateId)</param>
+        /// <param name="rowKeyPrefix">Customizable row-key prefix</param>
+        /// <returns></returns>
         public static Partition GetPartition(this CloudTable table, string partitionKey, string rowKeyPrefix)
         {
             return new Partition(table, partitionKey, rowKeyPrefix);
@@ -29,6 +42,13 @@ namespace OS.Events.Streamstone
             return streamResult.Found ? streamResult.Stream : new Stream(partition);
         }
 
+        /// <summary>
+        /// Persists a batch of events to storage
+        /// </summary>
+        /// <typeparam name="TState"></typeparam>
+        /// <param name="stream"></param>
+        /// <param name="aggregate"></param>
+        /// <returns></returns>
         public static async Task Invoke<TState>(this Stream stream, Func<TState, IEnumerable<IEvent>> aggregate)
             where TState : BaseState, new()
         {
@@ -37,6 +57,12 @@ namespace OS.Events.Streamstone
             await Stream.WriteAsync(stream, aggregate(latestState).Select(Event).ToArray());
         }
 
+        /// <summary>
+        /// Read all events from the stream in a batched manner. Apply them to a state and return the recreated latest state.
+        /// </summary>
+        /// <typeparam name="TState">The state</typeparam>
+        /// <param name="stream">The event stream from a partition</param>
+        /// <returns>Latest aggregate state</returns>
         public static async Task<TState> ReadLatestAsync<TState>(this Stream stream)
             where TState : BaseState, new()
         {
