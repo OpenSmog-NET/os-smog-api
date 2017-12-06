e79b1a8a113af6ad70b2390ea0c7b31d6b6e2760
Maciej Misztal <mmisztal1980@gmail.com>
2017-12-06 01:12:49 +0100

    debug info removed

--- src/OS.Smog.Api/Startup.Logging.cs
+++ src/OS.Smog.Api/Startup.Logging.cs
@@ -2,7 +2,6 @@
 using Microsoft.Extensions.Logging;
 using Serilog;
 using Serilog.Events;
-using System;
 
 namespace OS.Smog.Api
 {
@@ -10,17 +9,6 @@ namespace OS.Smog.Api
     {
         public static ILoggerFactory ConfigureLogging(this ILoggerFactory loggerFactory, IConfiguration configuration)
         {
-            var sinks = configuration.GetSection("Serilog").GetSection("WriteTo").GetChildren();
-            foreach (var sink in sinks)
-            {
-                Console.WriteLine(sink["Name"]);
-                var args = sink.GetSection("Args").GetChildren();
-                foreach (var arg in args)
-                {
-                    Console.WriteLine($"{arg.Key} : {arg.Value} [{arg.Value.Length} chars]");
-                }
-            }
-
             loggerFactory.AddConsole(configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
             Log.Logger = new LoggerConfiguration()
