14049a2ebb02e4d4e2ea37cc5503463651cb42fe
Maciej Misztal <mmisztal1980@gmail.com>
2017-12-19 01:00:51 +0100

    refactoring

--- test/OS.DAL.PgSql.IntegrationTests/TestCollection.cs
+++ test/OS.DAL.PgSql.IntegrationTests/TestCollection.cs
@@ -2,11 +2,6 @@
 
 namespace OS.DAL.PgSql.IntegrationTests
 {
-    public static class Constants
-    {
-        public const string IntegrationTestsCollection = "IntegrationTests";
-    }
-
     [CollectionDefinition(Constants.IntegrationTestsCollection)]
     public class TestCollection : ICollectionFixture<PostgresFixture>
     {
